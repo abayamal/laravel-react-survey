7da568f767e82e13666c964bf0258564f86f2b04
abayamal <abayamalsathira@gmil.com>
2024-11-18 18:12:38 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import { BeakerIcon } from "@heroicons/react/24/solid";
+
+function App() {
+  return (
+    <div className="bg-purple-400">
+      App.jsx
+      <BeakerIcon className="w-4" />
+    </div>
+  );
+}
+
+export default App;
