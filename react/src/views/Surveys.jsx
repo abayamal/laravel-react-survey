91bb67bcd767ba2881beac7dbc94fea318a1e6bc
Sathira Abayamal <sathiraA@icbtcampus.local>
2024-11-20 16:21:35 +0530

    1.00.41 layout end

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Surveys() {
-  return <div>Surveys</div>;
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Surveys
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8"></div>
+      </main>
+    </>
+  );
 }
