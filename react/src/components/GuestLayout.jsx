bc4769a27ec295bd8d2f1c9ead965a858d34c830
sathiraa <sathiraa@icbtcampus.edu.lk>
2024-11-25 18:52:48 +0530

    create survey list item

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+  const { currentUser, userToken } = useStateContext();
+
+  if (userToken) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import PageComponent from "../components/PageComponent";
+import { useStateContext } from "../contexts/ContextProvider";
+import SurveyListItem from "../components/SurveyListItem";
 
 export default function Surveys() {
-  return <PageComponent title="Surveys">Surveys content</PageComponent>;
+  const { surveys } = useStateContext();
+
+  console.log(surveys);
+  return (
+    <PageComponent title="Surveys">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+        {surveys.map((survey) => (
+          <SurveyListItem survey={survey} key={survey.key} />
+        ))}
+      </div>
+    </PageComponent>
+  );
 }
