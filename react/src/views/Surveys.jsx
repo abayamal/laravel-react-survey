d837132935297dfc27b8cf98f10939fa9dece914
sathiraa <sathiraa@icbtcampus.edu.lk>
2024-11-26 21:57:45 +0530

    survey list item

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -7,11 +7,19 @@ export default function Surveys() {
   const { surveys } = useStateContext();
 
   console.log(surveys);
+
+  const onDeleteClick = () => {
+    console.log("On delete click");
+  };
   return (
     <PageComponent title="Surveys">
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {surveys.map((survey) => (
-          <SurveyListItem survey={survey} key={survey.key} />
+          <SurveyListItem
+            survey={survey}
+            key={survey.key}
+            onDeleteClick={onDeleteClick}
+          />
         ))}
       </div>
     </PageComponent>
